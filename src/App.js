58a02c6e1cc89c77042dcd1f1b7c1937ac11560f
Kristina Taing <kataing12@gmail.com>
2020-01-31 16:01:29 -0800

    [prog] moved map into card-list component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { CardList } from './components/card-list/card-list.component'
 import './App.css';
 
 class App extends React.Component {
@@ -18,9 +19,7 @@ class App extends React.Component {
   render() {
     return (
       <div className="App">
-        {
-          this.state.monsters.map(monster => <h1 key={monster.id}> { monster.name } </h1>)
-        }
+        <CardList monsters={this.state.monsters} />
       </div>
     );
   }
