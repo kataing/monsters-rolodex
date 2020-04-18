856281f3a6c1e93918a8341a0976eb34497efd8a
Kristina Taing <kataing12@gmail.com>
2020-04-18 14:44:50 -0700

    [prog] added live search feature

--- src/App.js
+++ src/App.js
@@ -13,15 +13,23 @@ class App extends React.Component {
 
   componentDidMount() {
     fetch('https://jsonplaceholder.typicode.com/users')
-    .then(response => response.json())
-    .then(users => this.setState({ monsters: users }))
-    }
+      .then(response => response.json())
+      .then(users => this.setState({ monsters: users }))
+  }
 
   render() {
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter(monster => 
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+      );
     return (
       <div className="App">
-        <input type='search' placeholder='search monsters' onChange={e => this.setState({ searchField: e.target.value})} />
-        <CardList monsters={this.state.monsters} />
+        <input
+          type='search'
+          placeholder='search monsters'
+          onChange={e => this.setState({ searchField: e.target.value })}
+        />
+        <CardList monsters={filteredMonsters} />
       </div>
     );
   }
