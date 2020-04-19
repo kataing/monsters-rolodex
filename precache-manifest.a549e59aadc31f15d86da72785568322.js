c6f04a7129ecb2ab04f458fe6676b0b96af1d26e
Kristina Taing <kataing12@gmail.com>
2020-04-18 18:50:33 -0700

    Updates

---
+++ precache-manifest.a549e59aadc31f15d86da72785568322.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6adaf267421677490c92da1c6c18670",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "eae09cf05a554a933ecf",
+    "url": "/monsters-rolodex/static/css/main.6c9f9c0e.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "eae09cf05a554a933ecf",
+    "url": "/monsters-rolodex/static/js/main.50dab0f0.chunk.js"
+  },
+  {
+    "revision": "517fd9319d43d3a57612",
+    "url": "/monsters-rolodex/static/js/runtime-main.ae75c310.js"
+  }
+]);
